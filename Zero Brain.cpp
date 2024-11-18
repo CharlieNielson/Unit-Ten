60598447247634d33a8998af159fd6124cc5b8c6
CharlieNielson <charlienielson@mail.weber.edu>
2024-11-18 09:18:29 -0700

    Update Zero Brain.cpp

--- Zero Brain.cpp
+++ Zero Brain.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "thing.h"
+using namespace std;
 
+
+int main() {
+    
+    Derived1 d1;
+    Derived2 d2;
+    Base* ptr;
+    ptr = &d1;
+    ptr -> show();
+    ptr = &d2;
+    ptr -> show();
+    
+
+
+
+    return 17;
+}
