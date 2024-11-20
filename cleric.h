3532533e97c5462296667c471123e7129154ae5f
CharlieNielson <charlienielson@mail.weber.edu>
2024-11-20 14:38:33 -0700

    Update cleric.h

--- cleric.h
+++ cleric.h
@@ -1,1 +1,25 @@
+#pragma once
+#include <iostream>
+#include "Stuff.h"
+using namespace std;
 
+class cleric: public Stuff {
+    public:
+        cleric(string, int, int, int, int, int, int, int, int);
+
+    private:
+        const string spellList[10] = {"Cure Wounds", "Shield of Faith", "Bless", "Guidance", "Light", "Dispel Magic", "Clairvoyance", "Aid", "Silence", "Lesser Restoration"};
+};
+
+cleric::cleric(string n, int s1, int s2, int s3, int s4, int s5, int h, int m, int a) {
+    Stuff::name = n;
+    Stuff::spells[0] = spellList[s1 - 1];
+    Stuff::spells[1] = spellList[s2 - 1];
+    Stuff::spells[2] = spellList[s3 - 1];
+    Stuff::spells[3] = spellList[s4 - 1];
+    Stuff::spells[4] = spellList[s5 - 1];
+    Stuff::hp = h;
+    Stuff::mp = m;
+    Stuff::ac = a;
+    Stuff::clas = "Cleric";
+}
